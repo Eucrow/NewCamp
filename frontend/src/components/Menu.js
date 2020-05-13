23ab7c6f09f42e98027f184423900ba532af87eb
Marco Antonio Ámez Fernández <marco.a.amez@gmail.com>
2020-05-13 12:31:18 +0200

    Add links in menu

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -4,9 +4,13 @@ export default class Menu extends Component {
     render() {
         return (
             <ul>
-                <li><a href="index.html">Home</a></li>
+                <li><a href="select_survey.html">Select Survey</a></li>
                 <li><a href="surveys.html">Survey</a></li>
-                <li><a href="species.html">Species</a></li>
+                <li><a href="stations.html">Survey</a></li>
+                <li><a href="hauls.html">Survey</a></li>
+                <li><a href="weights.html">Survey</a></li>
+                <li><a href="samples.html">Survey</a></li>
+                <li><a href="species.html">Survey</a></li>
             </ul>
         );
     }
