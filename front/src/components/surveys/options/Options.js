bed1e4c496fd36b4bb3383ffb63b0022b52317fb
Marco Antonio Ámez Fernández <marco.a.amez@gmail.com>
2020-05-15 22:24:41 +0200

    Link surveys view with survey detail view

--- front/src/components/surveys/options/Options.js
+++ front/src/components/surveys/options/Options.js
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
 
+
+import { Link } from "react-router-dom";
+
 class ComponentsSurveysOptions extends Component {
+    /**
+	 * Component with option of every survey
+	 * @param {number} props.survey_id
+	 */
     constructor(props) {
         super(props);
         this.state = { 
             data: []
          }
+        this.api = "/Survey/" + this.props.survey_id;
     }
+    
     render() { 
         return ( 
             <ul>
-                <li>detail</li>
+                <li><Link to={this.api}> detail </Link></li>
                 <li>edit</li>
                 <li>remove</li>
                 <li>select</li>
