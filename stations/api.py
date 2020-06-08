b78156a72ffd7edb24d78c426d4f2d1bed92a69f
Marco Antonio Ámez Fernández <marco.a.amez@gmail.com>
2020-06-08 13:09:00 +0200

    Create edit and new station apis.

--- stations/api.py
+++ stations/api.py
@@ -1,6 +1,7 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from django.shortcuts import get_object_or_404
 from stations.models import Station
 from stations.serializers import StationSerializer
@@ -10,7 +11,25 @@ class StationsAPI(ListAPIView):
     serializer_class = StationSerializer
 
 class StationAPI(APIView):
+
     def get(self, request, station_id):
         station = get_object_or_404(Station, pk=station_id)
         serializer = StationSerializer(station)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = StationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(survey_id=request.data["survey_id"])
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        else:
+            return Response(status=HTTP_400_BAD_REQUEST)
+
+    def put(self, request, station_id):
+        station = get_object_or_404(Station, pk=station_id)
+        serializer = StationSerializer(station, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        else:
+            return Response(status=HTTP_400_BAD_REQUEST)
