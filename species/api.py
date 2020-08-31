824711bfe2904311f4d386bb9067e3a9bf238a17
Marco Antonio Ámez Fernández <marco.a.amez@gmail.com>
2020-08-31 11:20:31 +0200

    Add CategorySpeciesAPI and its url.

--- species/api.py
+++ species/api.py
@@ -4,8 +4,8 @@ from rest_framework.response import Response
 
 from django.shortcuts import get_object_or_404
 
-from species.models import Sp
-from species.serializers import SpeciesSerializer, SpNameSerializer
+from species.models import Sp, Category
+from species.serializers import SpeciesSerializer, SpNameSerializer, CategorySerializer
 
 
 class SpeciesListAPI(APIView):
@@ -15,7 +15,6 @@ class SpeciesListAPI(APIView):
         return Response(serializer.data)
 
 
-# working with class-based views
 class SpAPI(APIView):
     def get(self, request, pk):
         sp = Sp.objects.filter(pk=pk)
@@ -39,6 +38,7 @@ class SpAPI(APIView):
         else:
             return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
+
 class SpeciesGroupAPI(APIView):
     """
     Manage species with group-species variables instead of pk.
@@ -46,6 +46,16 @@ class SpeciesGroupAPI(APIView):
     def get(self, request, group):
         sp = Sp.objects.filter(group=group)
         serializer = SpNameSerializer(sp, many=True)
+        return Response(serializer.data)
+
+
+class CategorySpeciesAPI(APIView):
+    """
+    Manage caterories of one species.
+    """
+    def get(self, request, sp_id):
+        categories = Category.objects.filter(sp_id=sp_id)
+        serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data)
 
 
