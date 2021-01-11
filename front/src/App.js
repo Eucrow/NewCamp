960e53094c01c3997989af5bfaac2e1d53d5b270
Marco Antonio Ámez Fernández <marco.a.amez@gmail.com>
2021-01-11 12:18:40 +0100

    Clean useless code.

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,9 @@
 import React, { Fragment, useState } from "react";
 import "./index.css";
 
-import {BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import SurveyContext  from "./contexts/SurveyContext.js";
+import SurveyContext from "./contexts/SurveyContext.js";
 
 import ComponentsSurveys from "./components/surveys/Surveys.js";
 import ComponentsSurvey from "./components/surveyDetails/Survey.js";
@@ -19,97 +19,84 @@ import ComponentHaul from "./components/haulDetail/haul.js";
 import ComponentsHaulNew from "./components/haulNew/HaulNew.js";
 
 import ComponentsTrawlCatches from "./components/trawlCatches/TrawlHaulCatches.js";
-import ComponentsTrawlCatchNew from "./components/trawlCatches/TrawlCatchNew.js";
-
 
 export default function App() {
-  const [surveySelector, setSurvey] = useState(null);
-  const value = { surveySelector, setSurvey };
+	const [surveySelector, setSurvey] = useState(null);
+	const value = { surveySelector, setSurvey };
 
-  const [surveyName, setSurveyName] = useState();
+	const [surveyName, setSurveyName] = useState();
 
-  function getSurveyName(survey_id){
-    fetch("http://127.0.0.1:8000/api/1.0/surveys/" + survey_id)
-    .then(response => {
-        return response.json();
-    })
-    .then(survey => {
-      return survey.description
-    })
-    .then(surveyName =>{
-      setSurveyName(surveyName)
-    })
-  }
+	function getSurveyName(survey_id) {
+		fetch("http://127.0.0.1:8000/api/1.0/surveys/" + survey_id)
+			.then((response) => {
+				return response.json();
+			})
+			.then((survey) => {
+				return survey.description;
+			})
+			.then((surveyName) => {
+				setSurveyName(surveyName);
+			});
+	}
 
-  return (
-    <SurveyContext.Provider value={value}>
-    <Router>
-      {/* { console.log (surveyName)} */}
-      <main>
-        <nav>
+	return (
+		<SurveyContext.Provider value={value}>
+			<Router>
+				{/* { console.log (surveyName)} */}
+				<main>
+					<nav>
+						{/* survey name */}
+						{/* if surveySelector is not null, get the name of the survey */}
+						{surveySelector === null ? "" : getSurveyName(surveySelector)}
+						{surveyName === undefined ? (
+							<div style={{ display: "inline", fontWeight: "bold" }}>not survey selected</div>
+						) : (
+							<div style={{ display: "inline", fontWeight: "bold", fontSize: "1.5em" }}>{surveyName}</div>
+						)}{" "}
+						-<Link to="/">Home</Link> -<Link to="/SurveySelect">Select Survey</Link> -
+						<Link to="/Surveys">Surveys</Link> -<Link to="/Strata">Strata</Link> -
+						<Link to="/Stations">Stations</Link> -<Link to="/Hauls">Hauls</Link> -
+						<Link to="/Weights">Weights</Link> -<Link to="/Samples">Samples</Link> -
+						<Link to="/Masters">Species</Link>
+					</nav>
+				</main>
 
-            {/* survey name */}
-            {/* if surveySelector is not null, get the name of the survey */}
-            {surveySelector === null?
-              "" :
-              getSurveyName(surveySelector)
-            }
+				<Route path="/" exact component={Home} />
 
-            {surveyName === undefined?
-              <div style={{display:"inline", fontWeight:"bold"}}>not survey selected</div> :
-              <div style={{display:"inline", fontWeight:"bold", fontSize:"1.5em"}}>{surveyName}</div>} -
-            
-            <Link to="/">Home</Link> - 
-            <Link to="/SurveySelect">Select Survey</Link> -
-            <Link to="/Surveys">Surveys</Link> - 
-            <Link to="/Strata">Strata</Link> - 
-            <Link to="/Stations">Stations</Link> - 
-            <Link to="/Hauls">Hauls</Link> - 
-            <Link to="/Weights">Weights</Link> - 
-            <Link to="/Samples">Samples</Link> - 
-            <Link to="/Masters">Species</Link>
-         </nav>
-      </main>
-      
-      <Route path="/" exact component={Home} />
+				<Route path="/SurveySelect" exact component={ComponentsSurveySelect} />
 
-      <Route path="/SurveySelect" exact component={ComponentsSurveySelect} />
+				<Route path="/Survey/new" exact component={ComponentsSurveyNew} />
+				<Route path="/Survey/survey/:survey_id" exact component={ComponentsSurvey} />
 
-      <Route path="/Survey/new" exact component={ComponentsSurveyNew} />
-      <Route path="/Survey/survey/:survey_id" exact component={ComponentsSurvey} />
-      
-      <Route path="/Surveys" exact component={ComponentsSurveys} />
-      
-      <Route path="/Stations" exact component={ComponentsStations} />
-      <Route path="/Stations/new" exact component={ComponentsStationNew} />
-      <Route path="/Stations/station/:station_id" exact component={ComponentsStation} />
+				<Route path="/Surveys" exact component={ComponentsSurveys} />
 
-      <Route path="/Hauls/new" exact component={ComponentsHaulNew} />
-      {/* TODO: CONSIDER IF THE NEXT PATH IS USEFULL */}
-      <Route path="/Hauls/:survey_id([0-9]+)" exact component={ComponentsHauls} />
-      <Route path="/Hauls/haul/:haul_id" exact component={ComponentHaul} />
-      <Route path="/Hauls" exact component={ComponentsHauls} />
+				<Route path="/Stations" exact component={ComponentsStations} />
+				<Route path="/Stations/new" exact component={ComponentsStationNew} />
+				<Route path="/Stations/station/:station_id" exact component={ComponentsStation} />
 
-      <Route path="/Catches/haul/:haul_id" exact component={ComponentsTrawlCatches} />
-      <Route path="/Catches/new/:haul_id" exact component={ComponentsTrawlCatchNew} />
+				<Route path="/Hauls/new" exact component={ComponentsHaulNew} />
+				{/* TODO: CONSIDER IF THE NEXT PATH IS USEFULL */}
+				<Route path="/Hauls/:survey_id([0-9]+)" exact component={ComponentsHauls} />
+				<Route path="/Hauls/haul/:haul_id" exact component={ComponentHaul} />
+				<Route path="/Hauls" exact component={ComponentsHauls} />
 
-      
-      {/* <Route path="/Weights" component={Weights} /> */}
-      {/* <Route path="/Samples" component={Samples} /> */}
-      {/* <Route path="/Species" component={Species} /> */}
-      
-    </Router>
-    </SurveyContext.Provider>
-  );
+				<Route path="/Catches/haul/:haul_id" exact component={ComponentsTrawlCatches} />
+
+				{/* <Route path="/Weights" component={Weights} /> */}
+				{/* <Route path="/Samples" component={Samples} /> */}
+				{/* <Route path="/Species" component={Species} /> */}
+			</Router>
+		</SurveyContext.Provider>
+	);
 }
 
 // Home Page
 const Home = () => (
-  <Fragment>
-    <h1>Home</h1>
-    <FakeText />
-  </Fragment>
-  );
+	<Fragment>
+		<h1>Home</h1>
+		<FakeText />
+	</Fragment>
+);
 // Surveys Page
 // const Surveys = () => (
 //   <Fragment>
@@ -133,18 +120,18 @@ const Home = () => (
 //   );
 // Weights Page
 const Weights = () => (
-  <Fragment>
-    <h1>Weights</h1>
-    <FakeText />
-  </Fragment>
-  );
+	<Fragment>
+		<h1>Weights</h1>
+		<FakeText />
+	</Fragment>
+);
 // Samples Page
 const Samples = () => (
-  <Fragment>
-    <h1>Samples</h1>
-    <FakeText />
-  </Fragment>
-  );
+	<Fragment>
+		<h1>Samples</h1>
+		<FakeText />
+	</Fragment>
+);
 // Species Page
 // const Species = () => (
 //   <Fragment>
@@ -154,7 +141,11 @@ const Samples = () => (
 //   );
 
 const FakeText = () => (
-  <p>
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  </p>
-  )
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+		magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+		pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+		laborum.
+	</p>
+);
--- front/src/components/trawlCatches/Category.js
+++ front/src/components/trawlCatches/Category.js
@@ -19,7 +19,6 @@ class ComponentCategory extends Component {
 
 		this.state = {
 			group: "",
-			// sp_code: "",
 			sp_id: "",
 			category: "",
 			weight: "",
@@ -28,7 +27,6 @@ class ComponentCategory extends Component {
 
 	renderContent = () => {
 		if (this.props.status_catch === "add") {
-			// console.log(this.props);
 			return (
 				<Fragment>
 					<td>
@@ -170,90 +168,6 @@ class ComponentCategory extends Component {
 	};
 
 	render() {
-		// const sampled_weight = this.props.this_catch.sampled_weight ? this.props.this_catch.sampled_weight : null;
-		// if (this.props.status_catch === "view" || this.props.status_catch === "") {
-		// 	return (
-		// 		<Fragment>
-		// 			<td>
-		// 				catch_id: {this.props.this_catch.id} -- {this.props.this_catch.group}{" "}
-		// 				{this.props.this_catch.sp_code}
-		// 			</td>
-		// 			<td>{this.props.this_catch.sp_name}</td>
-		// 			<td>{this.props.this_catch.category}</td>
-		// 			<td>{this.props.this_catch.weight}</td>
-		// 			<td>
-		// 				<SampledWeight
-		// 					sampled_weight_id={this.props.this_catch.sampled_weight_id}
-		// 					sampled_weight={sampled_weight}
-		// 					catch_id={this.props.this_catch.id}
-		// 					handleChangeSampledWeight={this.props.handleChangeSampledWeight}
-		// 					updateSampledWeight={this.props.updateSampledWeight}
-		// 					createSampledWeight={this.props.createSampledWeight}
-		// 				/>
-		// 			</td>
-		// 		</Fragment>
-		// 	);
-		// } else if (this.props.status_catch === "edit") {
-		// 	return (
-		// 		<Fragment>
-		// 			<td>
-		// 				<input type="hidden" id="haul_id" name="haul_id" value={this.props.this_catch.haul_id} />
-		// 				catch_id: {this.props.this_catch.id} --
-		// 				<input
-		// 					type="number"
-		// 					id="group"
-		// 					name="group"
-		// 					min="1"
-		// 					max="5"
-		// 					value={this.props.this_catch.group}
-		// 					onChange={this.props.handleChangeGroup(this.props.this_catch.id)}
-		// 				/>
-		// 				<select
-		// 					id="sp_code"
-		// 					name="sp_code"
-		// 					value={
-		// 						this.props.this_catch.sp_id +
-		// 						"--" +
-		// 						this.props.this_catch.sp_code +
-		// 						"--" +
-		// 						this.props.this_catch.sp_name
-		// 					}
-		// 					onChange={this.props.handleChangeSpecies(this.props.this_catch.id)}
-		// 				>
-		// 					<option>Select species...</option>
-		// 					{this.props.species.map((s) => {
-		// 						if (s.group === parseInt(this.props.this_catch.group)) {
-		// 							return (
-		// 								<option value={s.id + "--" + s.sp_code + "--" + s.sp_name}>
-		// 									{s.sp_code}-{s.sp_name}
-		// 								</option>
-		// 							);
-		// 						}
-		// 					})}
-		// 				</select>
-		// 			</td>
-		// 			<td></td>
-		// 			<td>
-		// 				<input
-		// 					type="number"
-		// 					id="category_id"
-		// 					name="category_id"
-		// 					value={this.props.this_catch.category}
-		// 					onChange={this.props.handleChangeCategory(this.props.this_catch.id)}
-		// 				/>
-		// 			</td>
-		// 			<td>
-		// 				<input
-		// 					type="number"
-		// 					id="weight"
-		// 					name="weight"
-		// 					value={this.props.this_catch.weight}
-		// 					onChange={this.props.handleChangeWeight(this.props.this_catch.id)}
-		// 				/>
-		// 			</td>
-		// 		</Fragment>
-		// 	);
-		// }
 		return this.renderContent();
 	}
 }
