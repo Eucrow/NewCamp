f801b61c13089077e675800667b9a945db7346a5
Marco Antonio Ámez Fernández <marco.a.amez@gmail.com>
2021-03-25 10:27:43 +0100

    Minor changes.

--- conn_r/api.py
+++ conn_r/api.py
@@ -7,11 +7,11 @@ from hauls.models import Haul
 from conn_r.serializers import DataTrawlHaulsConnR, DataStationsConnR
 
 class GetTrawlHaulsAPIConnR(APIView):
-    '''
+    """
     Get data hauls of certain survey.
     Created to use with campR.
-    '''
-
+    """
+    # TODO: returns hydrography data too
     def get(self, request, acronym):
         hauls = Haul.objects.filter(station__survey__acronym=acronym)
         serializer = DataTrawlHaulsConnR(hauls, many=True)
@@ -19,10 +19,10 @@ class GetTrawlHaulsAPIConnR(APIView):
 
 
 class GetDataStationsAPIConnR(APIView):
-    '''
-
-    '''
-
+    """
+    Retrieve data of all stations of a survey, including trawl and hydrography data hauls.
+    Created to use with campR.
+    """
     def get(self, request, acronym):
         stations = Station.objects.filter(survey__acronym=acronym)
         serializer = DataStationsConnR(stations, many=True)
