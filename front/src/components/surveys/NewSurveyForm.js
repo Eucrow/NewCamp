56134f0bd1fa251deaedff62df3aa8ab8d74b552
Marco Antonio Ámez Fernández <marco.a.amez@gmail.com>
2022-01-17 15:17:58 +0100

    Convert NewSurveyForm class component to hook component.

--- front/src/components/surveys/NewSurveyForm.js
+++ front/src/components/surveys/NewSurveyForm.js
@@ -1,4 +1,6 @@
-import React, { Component } from "react";
+import React, { Component, useState, useContext } from "react";
+
+import { Formik } from "formik";
 
 import SurveysContext from "../../contexts/SuverysContext";
 
@@ -8,66 +10,70 @@ import UiButtonSaveNewSurvey from "./UiButtonSaveNewSurvey";
 /**
  * Survey component
  */
-class NewSurveyForm extends Component {
-	static contextType = SurveysContext;
+// class NewSurveyForm extends Component {
+const NewSurveyForm = () => {
+	// static contextType = SurveysContext;
 
-	constructor(props) {
-		super(props);
+	const surveysContext = useContext(SurveysContext);
 
-		this.form = React.createRef();
+	const [survey, setSurvey] = useState({});
 
-		this.state = {
-			survey: [],
-		};
+	const formRef = React.createRef();
 
-		this.handleChangeNew = this.handleChangeNew.bind(this);
-		this.validate = this.validate.bind(this);
-	}
+	// constructor(props) {
+	// 	super(props);
+
+	// 	this.form = React.createRef();
+
+	// 	this.state = {
+	// 		survey: [],
+	// 	};
+
+	// 	this.handleChangeNew = this.handleChangeNew.bind(this);
+	// 	this.validate = this.validate.bind(this);
+	// }
 
 	/**
 	 * Validate field forms by the HTML Constrait API
 	 * @returns true when all fields are valid.
 	 */
-	validate() {
+	const validate = () => {
 		// Validate end date/start date
-		if (this.state.survey.start_date > this.state.survey.end_date) {
-			this.form.current.end_date.setCustomValidity(
+		if (survey.start_date > survey.end_date) {
+			formRef.current.end_date.setCustomValidity(
 				"End date must be later than start date"
 			);
 		} else {
-			this.form.current.end_date.setCustomValidity("");
+			formRef.current.end_date.setCustomValidity("");
 		}
 
-		return this.form.current.reportValidity();
-	}
+		return formRef.current.reportValidity();
+	};
 
 	/**
 	 * Manage fields change in 'survey' state.
 	 * This method is diferent to handleChange os Surveys component.
 	 * @param {event} e - Event.
 	 */
-	handleChangeNew(e) {
+	const handleChangeNew = (e) => {
 		const name = e.target.name;
 		const value = e.target.value;
 
-		this.setState({
-			survey: {
-				...this.state.survey,
+		setSurvey((survey) => {
+			return {
+				...survey,
 				[name]: value,
-			},
+			};
 		});
-	}
+	};
 
 	/**
 	 * Allow only type the number of digits of maxLength property
-	 * @param {object} object
+	 * @param {event} e - Event
 	 */
-	maxLengthCheck = (object) => {
-		if (object.target.value.length > object.target.maxLength) {
-			object.target.value = object.target.value.slice(
-				0,
-				object.target.maxLength
-			);
+	const maxLengthCheck = (e) => {
+		if (e.target.value.length > e.target.maxLength) {
+			e.target.value = e.target.value.slice(0, e.target.maxLength);
 		}
 	};
 
@@ -75,7 +81,7 @@ class NewSurveyForm extends Component {
 	 * Allow only type the number of digits of maxLength property with sign
 	 * @param {object} object
 	 */
-	maxLengthSignCheck = (object) => {
+	const maxLengthSignCheck = (object) => {
 		if (
 			object.target.value.length > object.target.maxLength &&
 			object.target.value.charAt(0) === "-"
@@ -96,17 +102,17 @@ class NewSurveyForm extends Component {
 	 * Prevent 'e' and '-' in numeric input
 	 * @param {e} onKeyDown event
 	 */
-	preventNegativeE = (e) => {
+	const preventNegativeE = (e) => {
 		(e.key === "e" || e.key === "-") && e.preventDefault();
 	};
 
-	renderContent() {
+	const renderContent = () => {
 		var content = "";
 
 		content = (
 			<form
 				className="wrapper"
-				ref={this.form}
+				ref={formRef}
 				onSubmit={(e) => e.preventDefault()}
 			>
 				<div className="survey__row">
@@ -117,7 +123,7 @@ class NewSurveyForm extends Component {
 							id="description"
 							name="description"
 							className="station_number"
-							onChange={(e) => this.handleChangeNew(e)}
+							onChange={handleChangeNew}
 							required
 							pattern="^[a-zA-Z0-9\s]{1,30}$"
 						/>
@@ -128,7 +134,7 @@ class NewSurveyForm extends Component {
 							type="text"
 							id="acronym"
 							name="acronym"
-							onChange={(e) => this.handleChangeNew(e)}
+							onChange={handleChangeNew}
 							required
 							pattern="^[\w|\d]{3}$"
 						/>
@@ -141,7 +147,7 @@ class NewSurveyForm extends Component {
 							type="date"
 							id="start_date"
 							name="start_date"
-							onChange={(e) => this.handleChangeNew(e)}
+							onChange={handleChangeNew}
 						/>
 					</span>
 					<span className="field">
@@ -150,7 +156,7 @@ class NewSurveyForm extends Component {
 							type="date"
 							id="end_date"
 							name="end_date"
-							onChange={(e) => this.handleChangeNew(e)}
+							onChange={handleChangeNew}
 						/>
 					</span>
 				</div>
@@ -161,12 +167,12 @@ class NewSurveyForm extends Component {
 							type="number"
 							id="width_x"
 							name="width_x"
-							onChange={(e) => this.handleChangeNew(e)}
+							onChange={handleChangeNew}
 							min="0"
 							max="180"
 							maxLength={3}
-							onInput={this.maxLengthCheck}
-							onKeyDown={this.preventNegativeE}
+							onInput={maxLengthCheck}
+							onKeyDown={preventNegativeE}
 						/>
 					</span>
 					<span className="field">
@@ -175,12 +181,12 @@ class NewSurveyForm extends Component {
 							type="number"
 							id="width_y"
 							name="width_y"
-							onChange={(e) => this.handleChangeNew(e)}
+							onChange={handleChangeNew}
 							min="0"
 							max="90"
 							maxLength={2}
-							onInput={this.maxLengthCheck}
-							onKeyDown={this.preventNegativeE}
+							onInput={maxLengthCheck}
+							onKeyDown={preventNegativeE}
 						/>
 					</span>
 					<span className="field">
@@ -191,11 +197,11 @@ class NewSurveyForm extends Component {
 							type="number"
 							id="origin_x"
 							name="origin_x"
-							onChange={(e) => this.handleChangeNew(e)}
+							onChange={handleChangeNew}
 							min="-180"
 							max="180"
 							maxLength={4}
-							onInput={this.maxLengthSignCheck}
+							onInput={maxLengthSignCheck}
 						/>
 					</span>
 					<span className="field">
@@ -206,11 +212,11 @@ class NewSurveyForm extends Component {
 							type="number"
 							id="origin_y"
 							name="origin_y"
-							onChange={(e) => this.handleChangeNew(e)}
+							onChange={handleChangeNew}
 							min="-90"
 							max="90"
 							maxLength={3}
-							onInput={this.maxLengthSignCheck}
+							onInput={maxLengthSignCheck}
 						/>
 					</span>
 				</div>
@@ -221,7 +227,7 @@ class NewSurveyForm extends Component {
 							type="text"
 							id="ship"
 							name="ship"
-							onChange={(e) => this.handleChangeNew(e)}
+							onChange={handleChangeNew}
 						/>
 					</span>
 					<span className="field">
@@ -230,7 +236,7 @@ class NewSurveyForm extends Component {
 							type="text"
 							id="hauls_duration"
 							name="hauls_duration"
-							onChange={(e) => this.handleChangeNew(e)}
+							onChange={handleChangeNew}
 						/>
 					</span>
 					<span className="field">
@@ -239,7 +245,7 @@ class NewSurveyForm extends Component {
 							type="text"
 							id="unit_sample"
 							name="unit_sample"
-							onChange={(e) => this.handleChangeNew(e)}
+							onChange={handleChangeNew}
 						/>
 					</span>
 					<span className="field">
@@ -249,11 +255,11 @@ class NewSurveyForm extends Component {
 						<select
 							id="stratification"
 							name="stratification"
-							onChange={(e) => this.handleChangeNew(e)}
+							onChange={handleChangeNew}
 						>
 							<option />
 
-							{this.context.stratifications.map((st, idx) => {
+							{surveysContext.stratifications.map((st, idx) => {
 								return (
 									<option value={st.id} key={idx}>
 										{st.id} - {st.stratification}
@@ -269,14 +275,14 @@ class NewSurveyForm extends Component {
 						type="text"
 						id="comment"
 						name="comment"
-						onChange={(e) => this.handleChangeNew(e)}
+						onChange={handleChangeNew}
 					/>
 				</div>
 				<div className="survey__row">
 					<div className="survey__cell survey__cell--right buttonsWrapper">
 						<UiButtonSaveNewSurvey
-							survey={this.state.survey}
-							validate={this.validate}
+							survey={survey}
+							validate={validate}
 						/>
 						<UiButtonCancelEditSurvey />
 					</div>
@@ -285,11 +291,13 @@ class NewSurveyForm extends Component {
 		);
 
 		return content;
-	}
+	};
 
-	render() {
-		return this.renderContent();
-	}
-}
+	return renderContent();
+
+	// render() {
+	// 	return this.renderContent();
+	// }
+};
 
 export default NewSurveyForm;
