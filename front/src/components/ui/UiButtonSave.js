683c6b5413671c74f86a4ed55ba52bc16f48bb09
Marco Antonio Ámez Fernández <marco.a.amez@gmail.com>
2022-03-09 10:55:42 +0100

    Clean code.

--- front/src/components/ui/UiButtonSave.js
+++ front/src/components/ui/UiButtonSave.js
@@ -1,25 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 /**
  * Component of button to add a new survey.
  * Used in any form to send it.
  * @param {object} props.survey - Survey object
  */
-// class UiButtonSave extends Component {
 const UiButtonSave = ({ buttonText }) => {
 	const renderedButton = (
 		<button type="submit" className="buttonsWrapper__button">
 			{buttonText}
 		</button>
 	);
-
-	// render() {
-	// 	let content = null;
-	// 	content = (
-	// 		<button className="buttonsWrapper__button">Save Survey</button>
-	// 	);
-
-	// 	return content;
-	// }
 
 	return renderedButton;
 };
