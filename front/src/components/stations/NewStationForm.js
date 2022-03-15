253b8d28045f18b7797762356fec0153f4467b51
Marco Antonio Ámez Fernández <marco.a.amez@gmail.com>
2022-03-15 15:47:32 +0100

    Change NewStationForm from class to function component.

--- front/src/components/stations/NewStationForm.js
+++ front/src/components/stations/NewStationForm.js
@@ -1,92 +1,88 @@
-import React, { Component } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import SelectedSurveyContext from "../../contexts/SelectedSuveryContext";
 
 import UiButtonSave from "../ui/UiButtonSave";
 import UiButtonCancel from "../ui/UiButtonCancel";
 
-class NewStationForm extends Component {
-	/**
-	 * @param {method} props.handleAdd
-	 * @param {method} props.createStation
-	 */
+/**
+ * New station form component
+ * @param {method} props.handleAdd
+ * @param {method} props.createStation
+ */
+const NewStationForm = ({ props }) => {
+	const selectedSurveyContext = useContext(SelectedSurveyContext);
 
-	static contextType = SelectedSurveyContext;
+	const [station, setStation] = useState();
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			station: {},
-		};
-
-		this.handleChange = this.handleChange.bind(this);
-	}
-
-	handleChange(e) {
+	const handleChange = (e) => {
 		const name = e.target.name;
 		const value = e.target.value;
 
-		this.setState({
+		setStation({
 			station: {
-				...this.state.station,
+				...station,
 				[name]: value,
 			},
 		});
-	}
+	};
 
-	componentDidMount() {
-		this.setState({
+	useEffect(() => {
+		setStation({
 			station: {
-				survey_id: this.context.selectedSurveyId,
+				...station,
+				survey_id: selectedSurveyContext.selectedSurveyId,
 			},
 		});
-	}
+	}, []);
 
-	render() {
-		return (
-			<form
-				className="wrapper form__row"
-				onSubmit={(e) => {
-					this.props.createStation(e, this.state.station);
-					this.props.handleAdd(false);
-				}}
-			>
-				<div className="form__row">
-					<div className="form__cell">
-						<label htmlFor="station">Station:</label>
-						<input
-							type="number"
-							min="0"
-							max="9999"
-							maxLength="4"
-							size={4}
-							className="station_number"
-							id="station"
-							name="station"
-							onChange={(e) => this.handleChange(e)}
-						/>
-					</div>
-					<div className="form__cell">
-						<label htmlFor="comment">Comment:</label>
-						<textarea
-							type="text"
-							id="comment"
-							name="comment"
-							rows={1}
-							size={1000}
-							onChange={(e) => this.handleChange(e)}
-						/>
-					</div>
+	const renderedContent = (
+		<form
+			className="wrapper form__row"
+			onSubmit={(e) => {
+				props.createStation(e, this.state.station);
+				props.handleAdd(false);
+			}}
+		>
+			<div className="form__row">
+				<div className="form__cell">
+					<label htmlFor="station">Station:</label>
+					<input
+						type="number"
+						min="0"
+						max="9999"
+						maxLength="4"
+						size={4}
+						className="station_number"
+						id="station"
+						name="station"
+						onChange={(e) => {
+							handleChange(e);
+						}}
+					/>
 				</div>
-				<div className="form__row">
-					<div className="survey__cell survey__cell--right buttonsWrapper">
-						<UiButtonSave buttonText={"Save Station"} />
-						<UiButtonCancel handleMethod={this.props.handleAdd} />
-					</div>
+				<div className="form__cell">
+					<label htmlFor="comment">Comment:</label>
+					<textarea
+						type="text"
+						id="comment"
+						name="comment"
+						rows={1}
+						size={1000}
+						onChange={(e) => handleChange(e)}
+					/>
 				</div>
-			</form>
-		);
-	}
-}
+			</div>
+			<div className="form__row">
+				<div className="survey__cell survey__cell--right buttonsWrapper">
+					<UiButtonSave buttonText={"Save Station"} />
+					<UiButtonCancel handleMethod={this.props.handleAdd} />
+				</div>
+			</div>
+		</form>
+	);
+
+	return renderedContent;
+};
 
 export default NewStationForm;
