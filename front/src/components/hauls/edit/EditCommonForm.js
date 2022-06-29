a911d8b0b1f47793f73d3c4fdb943a4c7a63b207
Marco Antonio Ámez Fernández <marco.a.amez@gmail.com>
2022-06-29 10:27:27 +0200

    Fix buttons in Haul component.

--- front/src/components/hauls/edit/EditCommonForm.js
+++ front/src/components/hauls/edit/EditCommonForm.js
@@ -33,108 +33,106 @@ class EditCommonForm extends Component {
 
 		return (
 			<form
+				className="form__row form--wide"
 				onSubmit={(e) =>
 					this.handleSubmit(e, haul.id, this.props.station_id)
 				}
 			>
-				<div className="form__row">
-					<label className="form__cell">
-						Haul:
-						<input
-							type="number"
-							name="haul"
-							id="haul"
-							className="input__noSpinner"
-							min="1"
-							max="99"
-							maxLength="2"
-							size={2}
-							value={haul.haul || ""}
-							onChange={(e) => {
-								this.context.handleChangeCommonHaul(e, haul.id);
-							}}
-						/>
-					</label>
-					<label className="form__cell">
-						Stratum:
-						<select
-							id="stratum_id"
-							name="stratum_id"
-							className="select__largeWidth"
-							value={this.props.haul.stratum_id || "choose"}
-							onChange={(e) => {
-								this.context.handleChangeStratum(e, haul.id);
-							}}
-						>
-							{this.context.strata.map((stratum) => {
-								return (
-									<option key={stratum.id} value={stratum.id}>
-										{stratum.stratum}
-									</option>
-								);
-							})}
-						</select>
-					</label>
-					<label className="form__cell">
-						Sampler:
-						<select
-							id="sampler_id"
-							name="sampler"
-							className="select__normalWidth"
-							disabled
-							value={this.props.haul.sampler_id || "choose"}
-							onChange={this.props.handleChangeNestedIds}
-						>
-							{this.context.samplers.map((sampler) => {
-								return (
-									<option key={sampler.id} value={sampler.id}>
-										{sampler.sampler}
-									</option>
-								);
-							})}
-						</select>
-					</label>
-					<label className="form__cell">
-						Gear:
-						<select
-							id="gear_id"
-							name="gear"
-							className="select__gear"
-							value={this.props.haul.gear_id || "choose"}
-							onChange={(e) => {
-								this.context.handleChangeGear(e, haul.id);
-							}}
-						>
-							{this.context.gears.map((gear) => {
-								return (
-									<option key={gear.id} value={gear.id}>
-										{gear.name}
-									</option>
-								);
-							})}
-						</select>
-					</label>
-					<label className="form__cell">
-						Valid:
-						<input
-							type="checkbox"
-							name="valid"
-							id="valid"
-							defaultChecked={haul.valid}
-							onChange={(e) => {
-								this.context.handleChangeCommonValid(haul.id);
-							}}
-						/>
-					</label>
-				</div>
-				<div className="form__row">
-					<HaulButtonBar
-						haul_id={this.props.haul.id}
-						edit={this.props.edit}
-						handleEdit={this.props.handleEdit}
-						deleteHaul={this.context.deleteHaul}
+				<label className="form__cell">
+					Haul:
+					<input
+						type="number"
+						name="haul"
+						id="haul"
+						className="input__noSpinner"
+						min="1"
+						max="99"
+						maxLength="2"
+						size={2}
+						value={haul.haul || ""}
+						onChange={(e) => {
+							this.context.handleChangeCommonHaul(e, haul.id);
+						}}
 					/>
-				</div>
+				</label>
+				<label className="form__cell">
+					Stratum:
+					<select
+						id="stratum_id"
+						name="stratum_id"
+						className="select__largeWidth"
+						value={this.props.haul.stratum_id || "choose"}
+						onChange={(e) => {
+							this.context.handleChangeStratum(e, haul.id);
+						}}
+					>
+						{this.context.strata.map((stratum) => {
+							return (
+								<option key={stratum.id} value={stratum.id}>
+									{stratum.stratum}
+								</option>
+							);
+						})}
+					</select>
+				</label>
+				<label className="form__cell">
+					Sampler:
+					<select
+						id="sampler_id"
+						name="sampler"
+						className="select__normalWidth"
+						disabled
+						value={this.props.haul.sampler_id || "choose"}
+						onChange={this.props.handleChangeNestedIds}
+					>
+						{this.context.samplers.map((sampler) => {
+							return (
+								<option key={sampler.id} value={sampler.id}>
+									{sampler.sampler}
+								</option>
+							);
+						})}
+					</select>
+				</label>
+				<label className="form__cell">
+					Gear:
+					<select
+						id="gear_id"
+						name="gear"
+						className="select__gear"
+						value={this.props.haul.gear_id || "choose"}
+						onChange={(e) => {
+							this.context.handleChangeGear(e, haul.id);
+						}}
+					>
+						{this.context.gears.map((gear) => {
+							return (
+								<option key={gear.id} value={gear.id}>
+									{gear.name}
+								</option>
+							);
+						})}
+					</select>
+				</label>
+				<label className="form__cell">
+					Valid:
+					<input
+						type="checkbox"
+						name="valid"
+						id="valid"
+						defaultChecked={haul.valid}
+						onChange={(e) => {
+							this.context.handleChangeCommonValid(haul.id);
+						}}
+					/>
+				</label>
+
+				<HaulButtonBar
+					haul_id={this.props.haul.id}
+					edit={this.props.edit}
+					handleEdit={this.props.handleEdit}
+					deleteHaul={this.context.deleteHaul}
+				/>
 			</form>
 		);
 	}
