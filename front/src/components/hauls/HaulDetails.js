f9f11550b5f3b2bb8c9d8392b93df098d4cc5206
Marco A. Ámez <marco.a.amez@gmail.com>
2023-05-12 14:24:37 +0200

    Recover previous data when haul detail edition is canceled.

--- front/src/components/hauls/HaulDetails.js
+++ front/src/components/hauls/HaulDetails.js
@@ -13,15 +13,25 @@ import UiButtonCancel from "../ui/UiButtonCancel";
 
 import UiButtonStatusHandle from "../ui/UiButtonStatusHandle";
 
-const HaulDetails = ({ haul, detail, setDetail, validateHaulSampler }) => {
+const HaulDetails = ({ haul, detail, setDetail }) => {
 	/**
-	 * View haul detail component.
-	 * @param {object} haul
-	 * @param {method} validateHaulSampler
-	 * @param {method} setDetail
+	 * Component to show the details of a haul. It is possible to edit the haul.
+	 * When the component is loaded, the data of the haul is fetched from the API and stored in the state and
+	 * stored in the backup state. This one is used to recovery the original data if the user cancel the changes.
+	 * @param {object} haul The haul object.
+	 * @param {boolean} detail Boolean to show the details of the haul.
+	 * @param {method} setDetail Method to set the boolean detail.
 	 */
 
 	const [thisHaul, setThisHaul] = useState({
+		...haul,
+		meteo: {},
+		trawl_characteristics: {},
+		hydrography_characteristics: {},
+		sampler: {},
+	});
+
+	const [backupThisHaul, setBackupThisHaul] = useState({
 		...haul,
 		meteo: {},
 		trawl_characteristics: {},
@@ -33,14 +43,12 @@ const HaulDetails = ({ haul, detail, setDetail, validateHaulSampler }) => {
 
 	const [fecthError, setFetchError] = useState("");
 
-	//TODO: optimize requests: the request only need to return station_id, meteo,
-	// trawl_characteristics and hydrograpy_characteristics
 	const apiTrawlHaul = "http://127.0.0.1:8000/api/1.0/haul/trawl/" + haul.id;
 	const apiHydrographyHaul = "http://127.0.0.1:8000/api/1.0/haul/hydrography/" + haul.id;
 
-	const handleChangeMeteorology = (event) => {
-		var name = event.target.name;
-		var value = event.target.value;
+	const handleChangeMeteorology = (e) => {
+		var name = e.target.name;
+		var value = e.target.value;
 
 		setThisHaul((prevHaul) => ({
 			...prevHaul,
@@ -50,9 +58,9 @@ const HaulDetails = ({ haul, detail, setDetail, validateHaulSampler }) => {
 			},
 		}));
 	};
-	const handleChangeTrawl = (event) => {
-		var name = event.target.name;
-		var value = event.target.value;
+	const handleChangeTrawl = (e) => {
+		var name = e.target.name;
+		var value = e.target.value;
 		setThisHaul((prevHaul) => ({
 			...prevHaul,
 			trawl_characteristics: {
@@ -62,9 +70,9 @@ const HaulDetails = ({ haul, detail, setDetail, validateHaulSampler }) => {
 		}));
 	};
 
-	const handleChangeHydrography = (event) => {
-		var name = event.target.name;
-		var value = event.target.value;
+	const handleChangeHydrography = (e) => {
+		var name = e.target.name;
+		var value = e.target.value;
 		setThisHaul((prevHaul) => ({
 			...prevHaul,
 			hydrography_characteristics: {
@@ -74,12 +82,10 @@ const HaulDetails = ({ haul, detail, setDetail, validateHaulSampler }) => {
 		}));
 	};
 
-	const handleSubmit = (event) => {
-		event.preventDefault();
+	const handleSubmit = (e) => {
+		e.preventDefault();
 
 		const apiHaul = haul.sampler_id === 1 ? apiTrawlHaul : haul.sampler_id === 2 ? apiHydrographyHaul : null;
-
-		// console.log(JSON.stringify(thisHaul));
 
 		fetch(apiHaul, {
 			method: "PUT",
@@ -92,6 +98,18 @@ const HaulDetails = ({ haul, detail, setDetail, validateHaulSampler }) => {
 				setEdit(false);
 			})
 			.catch((error) => console.log(error));
+	};
+
+	const handleCancel = (status) => {
+		setThisHaul((prevHaul) => {
+			return {
+				...prevHaul,
+				meteo: backupThisHaul["meteo"],
+				trawl_characteristics: backupThisHaul["trawl_characteristics"],
+				hydrography_characteristics: backupThisHaul["hydrography_characteristics"],
+			};
+		});
+		setEdit(status);
 	};
 
 	useEffect(() => {
@@ -113,6 +131,7 @@ const HaulDetails = ({ haul, detail, setDetail, validateHaulSampler }) => {
 				})
 				.then((haul) => {
 					setThisHaul(haul);
+					setBackupThisHaul(haul);
 				});
 		}
 	}, [detail, apiHydrographyHaul, apiTrawlHaul, haul.sampler_id]);
@@ -166,7 +185,8 @@ const HaulDetails = ({ haul, detail, setDetail, validateHaulSampler }) => {
 							<div className="form__cell form__cell--right">
 								<div className="buttonsWrapper">
 									<UiButtonSave buttonText="Save Haul" />
-									<UiButtonCancel buttonText="Cancel" handleMethod={setEdit} />
+									{/* <UiButtonCancel buttonText="Cancel" handleMethod={setEdit} /> */}
+									<UiButtonCancel buttonText="Cancel" handleMethod={handleCancel} />
 								</div>
 							</div>
 						</div>
@@ -216,264 +236,5 @@ const HaulDetails = ({ haul, detail, setDetail, validateHaulSampler }) => {
 
 	return renderContent();
 };
-
-// class HaulDetails extends Component {
-// 	/**
-// 	 * View haul detail component.
-// 	 * @param {object} haul
-// 	 * @param {method} validateHaulSampler
-// 	 * @param {method} handleDetail
-// 	 */
-
-// 	constructor(props) {
-// 		super(props);
-
-// 		this.state = {
-// 			haul: {
-// 				meteo: {},
-// 				trawl_characteristics: {},
-// 				hydrography_characteristics: {},
-// 				sampler: {},
-// 			},
-// 			edit: false,
-// 		};
-
-// 		//TODO: optimize requests: the request only need to return station_id, meteo,
-// 		// trawl_characteristics and hydrograpy_characteristics
-// 		this.apiTrawlHaul = "http://127.0.0.1:8000/api/1.0/haul/trawl/" + haul.id;
-// 		this.apiHydrographyHaul = "http://127.0.0.1:8000/api/1.0/haul/hydrography/" + haul.id;
-
-// 		setEdit = setEdit.bind(this);
-// 		this.handleChangeMeteorology = this.handleChangeMeteorology.bind(this);
-// 		this.handleChangeTrawl = this.handleChangeTrawl.bind(this);
-// 		this.handleChangeHydrography = this.handleChangeHydrography.bind(this);
-// 		this.handleSubmit = this.handleSubmit.bind(this);
-
-// 		this.renderContent = this.renderContent.bind(this);
-// 	}
-
-// 	changeIsEdit(edit) {
-// 		this.setState(() => {
-// 			return {
-// 				edit: edit,
-// 			};
-// 		});
-// 	}
-
-// 	handleChangeMeteorology(event) {
-// 		const name = event.target.name;
-// 		const value = event.target.value;
-
-// 		const newHaulMeteo = update(haul, {
-// 			meteo: {
-// 				[name]: { $set: value },
-// 			},
-// 		});
-
-// 		this.setState({
-// 			haul: newHaulMeteo,
-// 		});
-// 	}
-
-// 	handleChangeTrawl(event) {
-// 		const name = event.target.name;
-// 		const value = event.target.value;
-
-// 		const newHaulTrawl = update(haul, {
-// 			trawl_characteristics: {
-// 				[name]: { $set: value },
-// 			},
-// 		});
-
-// 		this.setState({
-// 			haul: newHaulTrawl,
-// 		});
-// 	}
-
-// 	handleChangeHydrography(event) {
-// 		const name = event.target.name;
-// 		const value = event.target.value;
-
-// 		const newHaulHydrography = update(haul, {
-// 			hydrography_characteristics: {
-// 				[name]: { $set: value },
-// 			},
-// 		});
-
-// 		this.setState({
-// 			haul: newHaulHydrography,
-// 		});
-// 	}
-
-// 	handleSubmit(event) {
-// 		event.preventDefault();
-
-// 		const apiHaul =
-// 			haul.sampler_id === 1
-// 				? this.apiTrawlHaul
-// 				: haul.sampler_id === 2
-// 				? this.apiHydrographyHaul
-// 				: null;
-
-// 		console.log(JSON.stringify(haul));
-
-// 		fetch(apiHaul, {
-// 			method: "PUT",
-// 			headers: {
-// 				"Content-Type": "application/json",
-// 			},
-// 			body: JSON.stringify(haul),
-// 		})
-// 			.then(() => {
-// 				this.setState(() => {
-// 					return { edit: false };
-// 				});
-// 			})
-// 			.catch((error) => console.log(error));
-// 	}
-
-// 	componentDidMount() {
-// 		/**
-// 		 * When the component is mounted, get the sampler_id from props, fetch the complete
-// 		 * data of the haul and update the state with it.
-// 		 */
-// 		const apiHaul =
-// 			parseInt(haul.sampler_id) === 1
-// 				? this.apiTrawlHaul
-// 				: parseInt(haul.sampler_id) === 2
-// 				? this.apiHydrographyHaul
-// 				: null;
-
-// 		// Fetch haul.
-// 		fetch(apiHaul)
-// 			.then((response) => {
-// 				if (response.status > 400) {
-// 					return this.setState(() => {
-// 						return { placeholder: "Something went wrong!" };
-// 					});
-// 				}
-// 				return response.json();
-// 			})
-// 			.then((haul) => {
-// 				this.setState(() => {
-// 					return {
-// 						haul,
-// 					};
-// 				});
-// 			});
-// 	}
-
-// 	renderContent() {
-// 		if (haul.sampler_id === 1) {
-// 			if (edit === false) {
-// 				return (
-// 					<form className="form--wide" disabled>
-// 						<div className="form__row">
-// 							<ViewMeteorology haul={haul} />
-// 						</div>
-// 						<div className="form__row">
-// 							<ViewTrawl haul={haul} />
-// 						</div>
-// 						<div className="form__row">
-// 							<div className="form__cell form__cell--right">
-// 								<div className="buttonsWrapper">
-// 									<UiButtonStatusHandle
-// 										buttonText={"Hide detail"}
-// 										handleMethod={handleDetail}
-// 										newStatus={false}
-// 									/>
-
-// 									<UiButtonStatusHandle
-// 										buttonText={"Edit"}
-// 										handleMethod={setEdit}
-// 										newStatus={true}
-// 									/>
-// 								</div>
-// 							</div>
-// 						</div>
-// 					</form>
-// 				);
-// 			}
-
-// 			if (edit === true) {
-// 				return (
-// 					<form
-// 						className="form--wide"
-// 						onSubmit={(e) => {
-// 							this.handleSubmit(e);
-// 							setEdit(false);
-// 						}}
-// 					>
-// 						<div className="form__row">
-// 							<MeteorologyFormEdit
-// 								haul={haul}
-// 								handleChangeMeteorology={this.handleChangeMeteorology}
-// 							/>
-// 						</div>
-
-// 						<div className="form__row">
-// 							<TrawlFormEdit haul={haul} handleChangeTrawl={this.handleChangeTrawl} />
-// 						</div>
-
-// 						<div className="form__row">
-// 							<div className="form__cell form__cell--right">
-// 								<div className="buttonsWrapper">
-// 									<UiButtonSave buttonText="Save Haul" />
-// 									<UiButtonCancel buttonText="Cancel" handleMethod={setEdit} />
-// 								</div>
-// 							</div>
-// 						</div>
-// 					</form>
-// 				);
-// 			}
-// 		}
-
-// 		if (haul.sampler_id === 2) {
-// 			if (edit === false) {
-// 				return (
-// 					<div>
-// 						<HaulFormView haul={haul} />
-// 						<ViewHydrography haul={haul} />
-// 						<button type="submit" className="buttonsWrapper__button">
-// 							Save
-// 						</button>
-// 						<button
-// 							onClick={() => {
-// 								handleDetail(false);
-// 							}}
-// 						>
-// 							Hide detail
-// 						</button>
-// 					</div>
-// 				);
-// 			}
-
-// 			if (edit === true) {
-// 				return (
-// 					<div>
-// 						<EditHydrography
-// 							haul={haul}
-// 							handleChangeHydrography={this.handleChangeHydrography}
-// 						/>
-// 						<input type="submit" value="Save Haul" onClick={this.handleSubmit} />
-// 						<button
-// 							onClick={() => {
-// 								setEdit(false);
-// 							}}
-// 						>
-// 							Cancel Edition
-// 						</button>
-// 					</div>
-// 				);
-// 			}
-// 		}
-
-// 		return null;
-// 	}
-
-// 	render() {
-// 		return this.renderContent();
-// 	}
-// }
 
 export default HaulDetails;
