0be9851ceb2e192deae820ffc9af0d7931a7647e
Marco A. Ámez <marco.a.amez@gmail.com>
2023-07-20 09:18:39 +0200

    Delete useless folder.

--- frontend/src/components/common/Button.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class Link extends Component {
-    render() {
-        return (
-            <button type="button">{this.props.button_text}</button> 
-        );
-    }
-}
