e953a8a2404e338787f961e91d2524e36046e770
MARCO ANTONIO.AMEZ.FERNANDEZ.922048 <marco.amez@ieo.csic.es>
2024-09-16 19:26:56 +0200

    Clean code in catches serializer.

--- catches/serializers.py
+++ catches/serializers.py
@@ -30,12 +30,12 @@ class CatchesVerboseSerializer(serializers.ModelSerializer):
     increment = serializers.FloatField(source='sp.increment', read_only=True)
     sampled_weight = serializers.FloatField(
         source='samples.sampled_weight', required=False, read_only=True)
-    measurement_type_id = serializers.IntegerField(source='sp.measurement_type_id', read_only=True)
 
     class Meta:
         model = Catch
         fields = ['catch_id', 'category', 'weight', 'not_measured_individuals', 'haul', 'haul_id', 'group', 'sp_id',
-                  'sp_code', 'sp_name', 'unit', 'increment', 'sampled_weight', 'measurement_type_id', ]
+                  'sp_code', 'sp_name', 'unit', 'increment', 'sampled_weight',
+                  ]
 
 
 class SexCatchSerializer(serializers.ModelSerializer):
