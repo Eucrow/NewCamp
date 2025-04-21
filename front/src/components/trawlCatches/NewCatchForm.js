91688aaf0055dd80f30ba5c985524131dbc7f6f1
MARCO ANTONIO.AMEZ.FERNANDEZ.922048 <marco.amez@ieo.csic.es>
2025-04-21 12:17:01 +0200

    Clean code.

--- front/src/components/trawlCatches/NewCatchForm.js
+++ front/src/components/trawlCatches/NewCatchForm.js
@@ -3,42 +3,14 @@ import CatchButtonBar from "./CatchButtonBar";
 import GlobalContext from "../../contexts/GlobalContext";
 
 /**
- * CatchForm is a functional component that represents a form for managing catch data.
+ * CatchForm is a functional component that represents a empty form for adding catch data.
  *
  * @component
- * @param {string} catchStatus - The current status of the catch: "", "view" or "edit".
- * @param {object} thisCatch - The catch object that is currently being managed by this component.
- * @param {object} species - An object containing species data.
  * @param {function} createCatch - A function to create a new catch in the database.
- * @param {function} handleChangeGroup - A function to handle changes to the group field.
- * @param {function} handleChangeSpecies - A function to handle changes to the species field.
- * @param {function} handleChangeCategory - A function to handle changes to the category field.
- * @param {function} handleChangeWeight - A function to handle changes to the weight field.
- * @param {function} handleChangeSampledWeight - A function to handle changes to the sampled_weight field.
- * @param {function} updateCatch - A function to update the catch in the database.
- * @param {function} editCatchStatus - A function to manage the catchStatus state.
- * @param {string} catchId - The id of the catch.
- * @param {boolean} viewSexes - A boolean to manage the view of sexes.
- * @param {function} handleCancel - A function to handle cancel action.
- * @param {function} deleteCatch - A function to delete the catch from the database.
- * @param {function} handleViewSexes - A function to handle the view of sexes.
- * @param {function} handleChangeAdd - A function to handle changes to the add field.
+ * @param {function} handleChangeAdd - A function to manage the add state.
  * @returns {JSX.Element} The rendered Catch component.
  */
-const NewCatchForm = ({
-	catchStatus,
-	createCatch,
-	editCatchStatus,
-	handleChangeAdd,
-	handleChangeNotMeasuredIndividuals,
-}) => {
-	const [group, setGroup] = useState("");
-	const [sp_id, setSp_id] = useState("");
-	const [category, setCategory] = useState("");
-	const [weight, setWeight] = useState("");
-	const [sampled_weight, setSampled_weight] = useState("");
-	const [not_measured_individuals, setNot_measured_individuals] = useState("");
-
+const NewCatchForm = ({ createCatch, handleChangeAdd }) => {
 	const [new_catch, setNew_catch] = useState({
 		group: "",
 		sp_id: "",
@@ -53,10 +25,6 @@ const NewCatchForm = ({
 	const [style_species_invalid, setStyle_species_invalid] = useState("");
 
 	useEffect(() => {
-		setNew_catch({ group, sp_id, category, weight, sampled_weight });
-	}, [group, sp_id, category, weight, sampled_weight]);
-
-	useEffect(() => {
 		if (new_catch.sp_id === "") {
 			setStyle_species_invalid("species--invalid");
 		} else {
@@ -64,23 +32,26 @@ const NewCatchForm = ({
 		}
 	}, [new_catch.sp_id]);
 
+	const handleInputChange = (field, value) => {
+		setNew_catch((prev) => ({ ...prev, [field]: value }));
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		createCatch(new_catch);
+		setNew_catch({
+			group: "",
+			sp_id: "",
+			category: "",
+			weight: "",
+			sampled_weight: "",
+			// not_measured_individuals: "",
+		});
+	};
+
 	const renderContent = () => {
 		return (
-			<form
-				className="catches__table__row"
-				onSubmit={(e) => {
-					e.preventDefault();
-					createCatch(new_catch);
-					setNew_catch({
-						group: "",
-						sp_id: "",
-						category: "",
-						weight: "",
-						sampled_weight: "",
-						// not_measured_individuals: "",
-					});
-				}}
-			>
+			<form className="catches__table__row" onSubmit={handleSubmit}>
 				<input
 					value={new_catch.group}
 					className="catches__table__cell catches__table__group"
@@ -91,9 +62,7 @@ const NewCatchForm = ({
 					name="group"
 					min="1"
 					max="5"
-					onChange={(e) => {
-						setNew_catch({ ...new_catch, group: e.target.value });
-					}}
+					onChange={(e) => handleInputChange("group", e.target.value)}
 					aria-label="Group"
 				/>
 				<select
@@ -104,9 +73,7 @@ const NewCatchForm = ({
 					required={true}
 					id="sp_code"
 					name="sp_code"
-					onChange={(e) => {
-						setNew_catch({ ...new_catch, sp_id: e.target.value });
-					}}
+					onChange={(e) => handleInputChange("sp_id", e.target.value)}
 					aria-label="Species"
 				>
 					<option>Select species...</option>
@@ -131,9 +98,7 @@ const NewCatchForm = ({
 					name="category"
 					min="1"
 					max="99"
-					onChange={(e) => {
-						setNew_catch({ ...new_catch, category: e.target.value });
-					}}
+					onChange={(e) => handleInputChange("category", e.target.value)}
 					aria-label="Category"
 				/>
 				<input
@@ -145,9 +110,7 @@ const NewCatchForm = ({
 					name="weight"
 					min="1"
 					max="99999999"
-					onChange={(e) => {
-						setNew_catch({ ...new_catch, weight: e.target.value });
-					}}
+					onChange={(e) => handleInputChange("weight", e.target.value)}
 					aria-label="Weight"
 				/>
 				<input
@@ -158,9 +121,7 @@ const NewCatchForm = ({
 					name="sampled_weight"
 					min="0"
 					max="99999999"
-					onChange={(e) => {
-						setNew_catch({ ...new_catch, sampled_weight: e.target.value });
-					}}
+					onChange={(e) => handleInputChange("sampled_weight", e.target.value)}
 					aria-label="Sampled weight"
 				/>
 				<input
@@ -171,9 +132,7 @@ const NewCatchForm = ({
 					name="individuals"
 					min="0"
 					max="99999999"
-					onChange={(e) => {
-						setNew_catch({ ...new_catch, not_measured_individuals: e.target.value });
-					}}
+					onChange={(e) => handleInputChange("individuals", e.target.value)}
 					aria-label="Not measured individuals"
 				/>
 				<CatchButtonBar catchStatus={"add"} handleChangeAdd={handleChangeAdd} />
