68c57e20b88292e3f1c72a84de3cb8b1711e0f12
Marco A. Ámez <marco.a.amez@gmail.com>
2025-07-09 12:55:30 +0200

    Protect form deletion of stratification in backend.

--- stratifications/api.py
+++ stratifications/api.py
@@ -1,7 +1,20 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from django.db.models import ProtectedError
 from stratifications.models import Stratification
 from stratifications.serializers import StratificationSerializer
 
 class StratificationViewSet(viewsets.ModelViewSet):
     queryset = Stratification.objects.all()
     serializer_class = StratificationSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        try:
+            self.perform_destroy(instance)
+        except ProtectedError:
+            return Response(
+                {"error": "Cannot delete stratification as it is referenced in strata."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        return Response(status=status.HTTP_204_NO_CONTENT)
