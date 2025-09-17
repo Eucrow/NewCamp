c55b6c2c80dd13563f0f3d32a277255c6e4ff487
Marco A. Ámez <marco.a.amez@gmail.com>
2025-09-17 12:06:58 +0200

    Fix reports.

--- reports/api.py
+++ reports/api.py
@@ -47,7 +47,7 @@ def get_base_survey_data(acronym, include_trawl=False, include_meteorology=False
     return merged_df, survey, stations
 
 
-def get_trawl_data(acronym):
+def get_trawl_data(acronym, include_meteorology=False):
     """
     Get trawl data (stations, hauls, trawls, meteorology) for a survey and return merged DataFrame.
     
@@ -65,61 +65,65 @@ def get_trawl_data(acronym):
     hauls = Haul.objects.filter(station__in=stations, sampler_type="trawl")
     hauls_df = pd.DataFrame(list(hauls.values()))
     # Merge stations and hauls dataframes
-    merged_hauls_df = pd.merge(base_df, hauls_df, left_on='id', right_on='station_id', how='left',
-                               suffixes=('_station', '_haul'))
+    merged_df = pd.merge(base_df, hauls_df, left_on='station_id', right_on='station_id', how='left',
+                         suffixes=('_station', '_haul'))
+    merged_df = merged_df.rename(columns={'id': 'haul_id'})
+
     # Get haul trawls
     trawls = HaulTrawl.objects.filter(haul_id__in=hauls)
     trawls_df = pd.DataFrame(list(trawls.values()))
-    merged_hauls_df = pd.merge(merged_hauls_df, trawls_df, left_on='id_haul', right_on='id', how='left',
-                               suffixes=('', '_trawl'))
+    merged_df = pd.merge(merged_df, trawls_df, left_on='haul_id', right_on='id', how='left',
+                         suffixes=('', '_trawl'))
+
     # Conditionally get and merge meteorology data
-    # if include_meteorology:        
-    #     meteorologies = Meteorology.objects.filter(haul_id__in=hauls)
-    #     meteorologies_df = pd.DataFrame(list(meteorologies.values()))
-    #     merged_hauls_df = pd.merge(merged_hauls_df, meteorologies_df, left_on='id_haul', right_on='id', how='left',
-    #                                suffixes=('', '_meteorology'))
+    if include_meteorology:
+        meteorologies = Meteorology.objects.filter(haul_id__in=hauls)
+        meteorologies_df = pd.DataFrame(list(meteorologies.values()))
+        merged_df = pd.merge(merged_df, meteorologies_df, left_on='haul_id', right_on='id', how='left',
+                             suffixes=('', '_meteorology'))
     # # Get catches
-    # catches = Catch.objects.filter(haul__in=hauls)
-    # catches_df = pd.DataFrame(
-    #     list(catches.values('id', 'category', 'weight', 'haul_id', 'sp_id', 'not_measured_individuals')))
+    catches = Catch.objects.filter(haul__in=hauls)
+    catches_df = pd.DataFrame(
+        list(catches.values('id', 'category', 'weight', 'haul_id', 'sp_id', 'not_measured_individuals')))
+    catches_df = catches_df.rename(columns={'id': 'catch_id'})
 
     # # Merge hauls and catches dataframes
-    # merged_df = pd.merge(merged_hauls_df, catches_df, left_on='id_haul', right_on='haul_id', how='left',
-    #                      suffixes=('', '_catch'))
-    # merged_df = merged_df.rename(columns={'id': 'catch_id'})
+    merged_df = pd.merge(merged_df, catches_df, left_on='haul_id', right_on='haul_id', how='left',
+                         suffixes=('', '_catch'))
 
     # # Get sampled weights
-    # sampled_weights = SampledWeight.objects.filter(catch__in=catches)
-    # sampled_weights_df = pd.DataFrame(list(sampled_weights.values('catch_id', 'sampled_weight')))
+    sampled_weights = SampledWeight.objects.filter(catch__in=catches)
+    sampled_weights_df = pd.DataFrame(list(sampled_weights.values('catch_id', 'sampled_weight')))
 
     # # Merge catches and sampled weights dataframes
-    # merged_df = pd.merge(merged_df, sampled_weights_df, left_on='catch_id', right_on='catch_id', how='left',
-    #                      suffixes=('', '_sampled'))
+    merged_df = pd.merge(merged_df, sampled_weights_df, left_on='catch_id', right_on='catch_id', how='left',
+                         suffixes=('', '_sampled'))
 
     # # Get sexes
-    # sexes = Sex.objects.filter(catch__in=catches)
-    # sexes_df = pd.DataFrame(list(sexes.values('id', 'catch_id', 'sex')))
-    # sexes_df = sexes_df.rename(columns={'id': 'sex_id'})
+    sexes = Sex.objects.filter(catch__in=catches)
+    sexes_df = pd.DataFrame(list(sexes.values('id', 'catch_id', 'sex')))
+    sexes_df = sexes_df.rename(columns={'id': 'sex_id'})
 
     # # Merge catches and sexes dataframes
-    # merged_df = pd.merge(merged_df, sexes_df, left_on='catch_id', right_on='catch_id', how='left')
+    merged_df = pd.merge(merged_df, sexes_df, left_on='catch_id', right_on='catch_id', how='left')
 
     # # Get lengths
-    # lengths = Length.objects.filter(sex__in=sexes)
-    # lengths_df = pd.DataFrame(list(lengths.values()))
+    lengths = Length.objects.filter(sex__in=sexes)
+    lengths_df = pd.DataFrame(list(lengths.values()))
 
     # # Merge catches and lengths dataframes
-    # merged_df = pd.merge(merged_df, lengths_df, left_on='sex_id', right_on='sex_id', how='left',
-    #                      suffixes=('', '_length'))
+    merged_df = pd.merge(merged_df, lengths_df, left_on='sex_id', right_on='sex_id', how='left',
+                         suffixes=('', '_length'))
 
     # # Get species
-    # species = Sp.objects.filter(id__in=catches.values_list('sp_id', flat=True))
-    # species_df = pd.DataFrame(list(species.values()))
+    species = Sp.objects.filter(id__in=catches.values_list('sp_id', flat=True))
+    species_df = pd.DataFrame(list(species.values()))
 
     # # Merge catches and species dataframes
-    # merged_df = pd.merge(merged_df, species_df, left_on='sp_id', right_on='id', how='left',
-    #                      suffixes=('', '_species'))
-    return merged_hauls_df
+    merged_df = pd.merge(merged_df, species_df, left_on='sp_id', right_on='id', how='left',
+                         suffixes=('', '_species'))
+
+    return merged_df
 
 
 def get_hydrography_data(acronym):
@@ -165,7 +169,8 @@ class ReportCampFaunaCSVApi(APIView):
             ['acronym', 'haul', 'group', 'code', 'weight', 'number_individuals'])
 
         # Get base data using helper function
-        merged_df = get_base_survey_data(acronym)
+        # merged_df = get_base_survey_data(acronym)
+        merged_df = get_trawl_data(acronym, include_meteorology=False)
 
         grouped_df = merged_df.groupby(['acronym', 'station', 'haul', 'group', 'sp_code', 'category']).agg({
             'weight': 'first',
@@ -220,7 +225,7 @@ class ReportCampFaunaCSVApi(APIView):
 class ReportCampLengthsCSVApi(APIView):
     def get(self, request, acronym):
         # Get base data using shared helper function
-        merged_df = get_base_survey_data(acronym)
+        merged_df = get_trawl_data(acronym)
 
         # Get measurements
         measurements = MeasurementType.objects.all()
@@ -258,7 +263,7 @@ class ReportCampLengthsCSVApi(APIView):
 
 class ReportCampHaulsCSVApi(APIView):
     def get(self, request, acronym):
-        merged_df = get_base_survey_data(acronym, include_trawl=True, include_meteorology=True)
+        merged_df = get_trawl_data(acronym, include_meteorology=True)
 
         # Create response
         response = HttpResponse(content_type='text/csv')
